0d5266fad68ad5144da232302b36836eee97bfb4
Adrien Python <adrien.python@gmail.com>
2014-08-09 00:03:40 +0200

    Minor formatting changes

--- Dropbox/example/example.c
+++ Dropbox/example/example.c
@@ -18,27 +18,27 @@ void displayMetadataList(drbMetadataList* list, char* title);
 
 int main (int argc, char **argv) {
     
-	int err;
+    int err;
     void* output;
     
     // Application key and secret. To get them, create an app here:
     // https://www.dropbox.com/developers/apps
-	char *c_key    = "your consumer key";  //< consumer key
-	char *c_secret = "your consumer secret";  //< consumer secret
+    char *c_key    = "your consumer key";  //< consumer key
+    char *c_secret = "your consumer secret";  //< consumer secret
     
     // User key and secret. Leave them NULL or set them with your AccessToken.
-	char *t_key    = NULL; //< access token key
-	char *t_secret = NULL;  //< access token secret
+    char *t_key    = NULL; //< access token key
+    char *t_secret = NULL;  //< access token secret
     
     // Global initialisation
-	drbInit();
-	
+    drbInit();
+    
     // Create a Dropbox client
-	drbClient* cli = drbCreateClient(c_key, c_secret, t_key, t_secret);
-	
+    drbClient* cli = drbCreateClient(c_key, c_secret, t_key, t_secret);
+    
     // Request a AccessToken if undefined (NULL)
-	if(!t_key || !t_secret) {
-		drbOAuthToken* reqTok = drbObtainRequestToken(cli);
+    if(!t_key || !t_secret) {
+        drbOAuthToken* reqTok = drbObtainRequestToken(cli);
         
         if (reqTok) {
             char* url = drbBuildAuthorizeUrl(reqTok);
@@ -60,8 +60,8 @@ int main (int argc, char **argv) {
             fprintf(stderr, "Failed to obtain a RequestToken...\n");
             return EXIT_FAILURE;
         }
-	}
-
+    }
+    
     // Set default arguments to not repeat them on each API call
     drbSetDefault(cli, DRBOPT_ROOT, DRBVAL_ROOT_AUTO, DRBOPT_END);
     
@@ -82,7 +82,7 @@ int main (int argc, char **argv) {
     err = drbGetMetadata(cli, &output,
                          DRBOPT_PATH, "/",
                          DRBOPT_LIST, true,
-    //                     DRBOPT_FILE_LIMIT, 100,
+                         //                     DRBOPT_FILE_LIMIT, 100,
                          DRBOPT_END);
     if (err != DRBERR_OK) {
         printf("Metadata error (%d): %s\n", err, (char*)output);
@@ -92,7 +92,7 @@ int main (int argc, char **argv) {
         displayMetadata(meta, "Metadata");
         drbDestroyMetadata(meta, true);
     }
-
+    
     // Try to download a file named hello.txt
     FILE *file = fopen("/tmp/hello.txt", "w"); // Write it in this file
     output = NULL;
@@ -102,7 +102,7 @@ int main (int argc, char **argv) {
                      DRBOPT_IO_FUNC, fwrite,
                      DRBOPT_END);
     fclose(file);
-
+    
     if (err != DRBERR_OK) {
         printf("Get File error (%d): %s\n", err, (char*)output);
         free(output);
@@ -117,7 +117,7 @@ int main (int argc, char **argv) {
         memStream stream; memStreamInit(&stream);
         stream.data = "hello world!";
         stream.size = strlen(stream.data);
-
+        
         err = drbPutFile(cli, NULL,
                          DRBOPT_PATH, "/hello.txt",
                          DRBOPT_IO_DATA, &stream,
@@ -125,12 +125,12 @@ int main (int argc, char **argv) {
                          DRBOPT_END);
         printf("File upload: %s\n", err ? "Failed" : "Successful");
     }
-
+    
     // Free all client allocated memory
     drbDestroyClient(cli);
-	
+    
     // Global Cleanup
-	drbCleanup();
+    drbCleanup();
     
     return EXIT_SUCCESS;
 }
@@ -139,64 +139,64 @@ int main (int argc, char **argv) {
 char* strFromBool(bool b) { return b ? "true" : "false"; }
 
 /*!
- * \brief	Display a drbAccountInfo item in stdout.
- * \param	info    account informations to display.
- * \return	void
+ * \brief  Display a drbAccountInfo item in stdout.
+ * \param  info    account informations to display.
+ * \return  void
  */
 void displayAccountInfo(drbAccountInfo* info) {
-	if(info) {
-		printf("---------[ Account info ]---------\n");
-		if(info->referralLink)         printf("referralLink: %s\n", info->referralLink);
-		if(info->displayName)          printf("displayName:  %s\n", info->displayName);
-		if(info->uid)                  printf("uid:          %d\n", *info->uid);
-		if(info->country)              printf("country:      %s\n", info->country);
-		if(info->email)                printf("email:        %s\n", info->email);
-		if(info->quotaInfo.datastores) printf("datastores:   %u\n", *info->quotaInfo.datastores);
-		if(info->quotaInfo.shared)     printf("shared:       %u\n", *info->quotaInfo.shared);
-		if(info->quotaInfo.quota)      printf("quota:        %u\n", *info->quotaInfo.quota);
-		if(info->quotaInfo.normal)     printf("normal:       %u\n", *info->quotaInfo.normal);
-	}
+    if(info) {
+        printf("---------[ Account info ]---------\n");
+        if(info->referralLink)         printf("referralLink: %s\n", info->referralLink);
+        if(info->displayName)          printf("displayName:  %s\n", info->displayName);
+        if(info->uid)                  printf("uid:          %d\n", *info->uid);
+        if(info->country)              printf("country:      %s\n", info->country);
+        if(info->email)                printf("email:        %s\n", info->email);
+        if(info->quotaInfo.datastores) printf("datastores:   %u\n", *info->quotaInfo.datastores);
+        if(info->quotaInfo.shared)     printf("shared:       %u\n", *info->quotaInfo.shared);
+        if(info->quotaInfo.quota)      printf("quota:        %u\n", *info->quotaInfo.quota);
+        if(info->quotaInfo.normal)     printf("normal:       %u\n", *info->quotaInfo.normal);
+    }
 }
 
 /*!
- * \brief	Display a drbMetadata item in stdout.
- * \param	meta    metadata to display.
+ * \brief  Display a drbMetadata item in stdout.
+ * \param  meta    metadata to display.
  * \param   title   display the title before the metadata.
- * \return	void
+ * \return  void
  */
 void displayMetadata(drbMetadata* meta, char* title) {
-	if (meta) {
-		if(title) printf("---------[ %s ]---------\n", title);
-		if(meta->hash)        printf("hash:        %s\n", meta->hash       );
-		if(meta->rev)         printf("rev:         %s\n", meta->rev        );
-		if(meta->thumbExists) printf("thumbExists: %s\n", strFromBool(*meta->thumbExists));
-		if(meta->bytes)       printf("bytes:       %d\n", *meta->bytes);
-		if(meta->modified)    printf("modified:    %s\n", meta->modified);
-		if(meta->path)        printf("path:        %s\n", meta->path);
-		if(meta->isDir)       printf("isDir:       %s\n", strFromBool(*meta->isDir));
-		if(meta->icon)        printf("icon:        %s\n", meta->icon       );
-		if(meta->root)        printf("root:        %s\n", meta->root       );
-		if(meta->size)        printf("size:        %s\n", meta->size       );
-		if(meta->clientMtime) printf("clientMtime: %s\n", meta->clientMtime);
-		if(meta->isDeleted)   printf("isDeleted:   %s\n", strFromBool(*meta->isDeleted));
-		if(meta->mimeType)    printf("mimeType:    %s\n", meta->mimeType);
-		if(meta->revision)    printf("revision:    %d\n", *meta->revision);
-		if(meta->contents)    displayMetadataList(meta->contents, "Contents");
-	}
+    if (meta) {
+        if(title) printf("---------[ %s ]---------\n", title);
+        if(meta->hash)        printf("hash:        %s\n", meta->hash);
+        if(meta->rev)         printf("rev:         %s\n", meta->rev);
+        if(meta->thumbExists) printf("thumbExists: %s\n", strFromBool(*meta->thumbExists));
+        if(meta->bytes)       printf("bytes:       %d\n", *meta->bytes);
+        if(meta->modified)    printf("modified:    %s\n", meta->modified);
+        if(meta->path)        printf("path:        %s\n", meta->path);
+        if(meta->isDir)       printf("isDir:       %s\n", strFromBool(*meta->isDir));
+        if(meta->icon)        printf("icon:        %s\n", meta->icon);
+        if(meta->root)        printf("root:        %s\n", meta->root);
+        if(meta->size)        printf("size:        %s\n", meta->size);
+        if(meta->clientMtime) printf("clientMtime: %s\n", meta->clientMtime);
+        if(meta->isDeleted)   printf("isDeleted:   %s\n", strFromBool(*meta->isDeleted));
+        if(meta->mimeType)    printf("mimeType:    %s\n", meta->mimeType);
+        if(meta->revision)    printf("revision:    %d\n", *meta->revision);
+        if(meta->contents)    displayMetadataList(meta->contents, "Contents");
+    }
 }
 
 /*!
- * \brief	Display a drbMetadataList item in stdout.
- * \param	list    list to display.
+ * \brief  Display a drbMetadataList item in stdout.
+ * \param  list    list to display.
  * \param   title   display the title before the list.
- * \return	void
+ * \return  void
  */
 void displayMetadataList(drbMetadataList* list, char* title) {
-	if (list){
-		printf("---------[ %s ]---------\n", title);
-		for (int i = 0; i < list->size; i++) {
+    if (list){
+        printf("---------[ %s ]---------\n", title);
+        for (int i = 0; i < list->size; i++) {
             
-			displayMetadata(list->array[i], list->array[i]->path);
-		}
-	}
+            displayMetadata(list->array[i], list->array[i]->path);
+        }
+    }
 }
